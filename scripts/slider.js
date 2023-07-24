fa234a965712571261b0ed73e3ad2e4878d9899b
Rekonir <zhed-nikita@mail.ru>
2023-07-24 16:25:19 +0300

    add slider animation + start work whith acc menu

---
+++ scripts/slider.js
@@ -0,0 +1,28 @@
+const sliderPositionArray = document.getElementsByClassName('sliderBox__position')
+const trackerPositionArray = document.getElementsByClassName('tracker')
+
+const changeSlide = () => {
+    let indexCurentSlider = 1
+    sliderPositionArray[indexCurentSlider-1].classList.add('sliderBox__position__active')
+    trackerPositionArray[indexCurentSlider-1].classList.add('tracker_active')
+
+    setInterval(() => {
+        if (indexCurentSlider !== 0) {
+          sliderPositionArray[indexCurentSlider-1].classList.remove('sliderBox__position__active')
+          sliderPositionArray[indexCurentSlider].classList.add('sliderBox__position__active')
+
+          trackerPositionArray[indexCurentSlider-1].classList.remove('tracker_active')
+          trackerPositionArray[indexCurentSlider].classList.add('tracker_active')
+        }
+        else {            
+          sliderPositionArray[sliderPositionArray.length - 1].classList.remove('sliderBox__position__active')
+          sliderPositionArray[indexCurentSlider].classList.add('sliderBox__position__active')
+
+          trackerPositionArray[sliderPositionArray.length - 1].classList.remove('tracker_active')
+          trackerPositionArray[indexCurentSlider].classList.add('tracker_active')
+        }
+        indexCurentSlider < (sliderPositionArray.length - 1) ? indexCurentSlider++ : indexCurentSlider = 0
+        console.log(indexCurentSlider, sliderPositionArray)
+    }, 10000)
+}
+changeSlide()
